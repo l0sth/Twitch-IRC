04af3328464d38d8b66c28eaa555e5bf999b8d02
Schmoopiie <Schmoopiie@gmail.com>
2014-09-13 19:38:58 -0400

    Same license everywhere.

--- javascript/twitch/servers.js
+++ javascript/twitch/servers.js
@@ -1,9 +1,5 @@
 /**
- * Twitch-IRC
- *
- * An open source library for Node.js to connect on Twitch IRC Servers.
- *
- * NOTICE OF LICENSE
+ * The MIT License (MIT)
  *
  * Copyright (c) 2014 Schmoopiie
  *
@@ -14,22 +10,16 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
- * @package  Twitch-IRC
- * @author   Schmoopiie
- * @license  http://opensource.org/licenses/MIT - The MIT License (MIT)
- * @link     http://www.schmoopiie.com
- * @since    version 1.0
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 var serverList = {
